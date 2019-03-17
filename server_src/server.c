55b9dea7664295eacad3e0c145bfef35ea54bae1
Andrii Makarenko <amakaren@student.unit.ua>
2019-03-17 20:47:38 +0200

    Server_isolated

--- server_src/server.c
+++ server_src/server.c
@@ -11,15 +11,9 @@
 /* ************************************************************************** */
 
 #include "../minitalk.h"
+#include <stdio.h>
 
 t_message g_message;
-
-// void	print_message(char *str)
-// {
-// 	ft_putstr("The message is received:\n");
-// 	ft_putstr(str);
-// 	ft_putchar('\n');
-// }
 
 char	*add_char(char *str, char c)
 {
@@ -42,15 +36,32 @@ void	get_new_message(void)
 	g_message.gbit = 0;
 }
 
-static void	update_char(int sig)
-{
-		int value;
+// static void	update_char(int sig)
+// {
+// 		int value;
+//
+// 		value = 0;
+// 		if (sig == SIGUSR1)
+// 			value = 1;
+// 		if (sig == SIGUSR1)
+// 			printf("Received 1\n");
+// 		if (sig == SIGUSR2)
+// 			printf("Received 2\n");
+// 		g_message.gchar ^= (-value ^ g_message.gchar) & (1UL << g_message.gbit);
+// 		g_message.gbit++;
+// 		printf("Char is now \'%c\'\n", g_message.gchar);
+// }
 
-		value = 0;
-		if (sig == SIGUSR1)
-			value = 1;
-		g_message.gchar ^= (-value ^ g_message.gchar) & (1UL << g_message.gbit);
-		g_message.gbit++;
+static void	handle_sig1(int sig)
+{
+	(void)sig;
+	printf("Received 1\n");
+}
+
+static void	handle_sig2(int sig)
+{
+	(void)sig;
+	printf("Received 2\n");
 }
 
 int		main(void)
@@ -61,8 +72,8 @@ int		main(void)
 	ft_putstr(ft_strjoin(ft_itoa(proc_id), \
 	" - my PID. If you enter anything other than exactly this, I won't work,"
 	" and that's a designed feature, not a bug.\n\n"));
-	signal(SIGUSR1, update_char);
-	signal(SIGUSR2, update_char);
+	signal(SIGUSR1, handle_sig1);
+	signal(SIGUSR2, handle_sig2);
 	get_new_message();
 	while (ft_strcmp(g_message.text, "exit"))
 	{

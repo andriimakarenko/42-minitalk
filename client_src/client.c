38937e28979b7bf0b6ce5ce10dcd0351bda718df
fullmetaldeus <fullmetaldeus@gmail.com>
2019-03-17 21:55:46 +0200

    client fix

--- client_src/client.c
+++ client_src/client.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client_src.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amakaren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pkuznets <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/16 01:24:39 by amakaren          #+#    #+#             */
-/*   Updated: 2019/03/16 01:24:39 by amakaren         ###   ########.fr       */
+/*   Created: 2019/03/17 21:49:24 by pkuznets          #+#    #+#             */
+/*   Updated: 2019/03/17 21:50:57 by pkuznets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-int is_numeric(const char *str)
+int		is_numeric(const char *str)
 {
 	int i;
 
@@ -23,14 +23,14 @@ int is_numeric(const char *str)
 			return (1);
 		i++;
 	}
-	return(0);
+	return (0);
 }
 
 void	send_symbol(int server_pid, unsigned char sym)
 {
 	uint8_t		i;
 
-	i = 1 << 6;
+	i = 1 << 7;
 	while (i)
 	{
 		if (sym & i)
@@ -64,13 +64,13 @@ void	sender(int server_pid, const char *message)
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
-	int i;
+	int	i;
 
 	if (argc != 3 || !(argv[1]))
 		return (ft_putstr_mod("Usage: \"./client_src server_PID message\"\n"));
 	if (!argv[2] || !argv[2][0])
 		return (ft_putstr_mod("Stop messin with me, \
-				enter a valid message\n"));
+		enter a valid message\n"));
 	if (is_numeric(argv[1]) != 0)
 		return (ft_putstr_mod("PID must comprise of numbers only\n"));
 	server_pid = (pid_t)ft_atoi(argv[1]);

391df73b49909719e456464b40deb503d96958e6
fullmetaldeus <fullmetaldeus@gmail.com>
2019-03-17 16:05:11 +0200

    is_numeric added

--- client/client.c
+++ client/client.c
@@ -14,20 +14,34 @@
 
 int	print_exit(char *str)
 {
-	__builtin_printf(str);
+	ft_putstr(str);
 	return (0);
+}
+
+int is_numeric(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	return(0);
 }
 
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
 
-	if (argc != 3)
+	if (argc != 3 || !(argv[1]))
 		return (print_exit("Usage: \"./client server_PID message\"\n"));
 	if (!argv[2] || !argv[2][0])
 		return (print_exit("Stop messin with me, \
 				enter a valid message\n"));
-	if (!(isnumeric(argv[1])))
+	if (is_numeric(argv[1]) != 0)
 		return (print_exit("PID must comprise of numbers only\n"));
 	server_pid = (pid_t);
 	if (server_pid < 0)
